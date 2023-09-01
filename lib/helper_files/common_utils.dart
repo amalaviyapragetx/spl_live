74e0a22cd0b7f313940d6d4378b21ee63d76299e
pragetx_ios01 <pragetx.ios01@gmail.com>
2023-09-01 11:20:00 +0530

    delete assets

--- lib/helper_files/common_utils.dart
+++ lib/helper_files/common_utils.dart
@@ -1,29 +1,6 @@
 import 'package:intl/intl.dart';
 
 class CommonUtils {
-  String formatDateTimeToDDMMYYYY(DateTime dateTime) {
-    final DateFormat formatter = DateFormat('dd-MM-yyyy');
-    String formattedDate = formatter.format(dateTime);
-    return formattedDate;
-  }
-
-  String formatStringDateToDDMMYYYY(String date) {
-    var inputFormat = DateFormat('yyyy-MM-dd');
-    var inputDate = inputFormat.parse(date);
-
-    final DateFormat formatter = DateFormat('dd-MM-yyyy');
-    String formattedDate = formatter.format(inputDate);
-    return formattedDate;
-  }
-
-  String formatStringDateToHHMMADDMMYYYY(String date) {
-    DateTime dateTime = DateTime.parse(date);
-
-    String formattedDateTimeString =
-        DateFormat('hh:mm a dd-MM-yyyy').format(dateTime);
-    return formattedDateTimeString;
-  }
-
   String formatStringToDDMMMYYYYHHMMSSA(String date) {
     DateTime dateTime = DateTime.parse(date);
     String formattedDateTimeString =
@@ -50,75 +27,10 @@ class CommonUtils {
     return difference;
   }
 
-  String formatDateToYYYYMMDD(DateTime dateTime) {
-    final DateFormat formatter = DateFormat('yyyy-MM-dd');
-    String formattedDate = formatter.format(dateTime);
-    return formattedDate;
-  }
-
   String formatStringToHHMMA(String time) {
     final parsedTime = DateFormat("HH:mm:ss").parse(time);
     final formattedTime = DateFormat("hh:mm a").format(parsedTime);
     return formattedTime;
-  }
-
-  String formatStringToHHMM(String time) {
-    final parsedTime = DateFormat("HH:mm:ss").parse(time);
-    final formattedTime = DateFormat("hh:mm").format(parsedTime);
-    return formattedTime;
-  }
-
-  String formatDateStringToDDMMMMYYYY(String date) {
-    var inputFormat = DateFormat('yyyy-MM-dd');
-    var inputDate = inputFormat.parse(date);
-
-    var outputFormat = DateFormat('dd MMMM yyyy');
-    var outputDate = outputFormat.format(inputDate);
-
-    return outputDate;
-  }
-
-  String formatDateStringToHHMMDDMMMMYYYY(String date) {
-    var inputFormat = DateFormat('yyyy-MM-dd hh:mm:ss');
-    var inputDate = inputFormat.parse(date);
-
-    var outputFormat = DateFormat('hh:mm a dd MMM yyyy');
-    var outputDate = outputFormat.format(inputDate);
-
-    return outputDate;
-  }
-
-  String formatDateStringToHHMMA(String date) {
-    DateTime utcTime = DateTime.parse(date);
-    String formattedTime = DateFormat('dd-MM-yyyy').format(utcTime.toLocal());
-    return formattedTime;
-  }
-
-  String formatDateStringToEEEEMMMMddyyyy(String date) {
-    DateTime utcTime = DateTime.parse(date);
-    String formattedTime =
-        DateFormat('EEEE, MMMM dd, yyyy').format(utcTime.toLocal());
-    return formattedTime;
-  }
-
-  String formatDateStringToMMMYYYY(String date) {
-    var inputFormat = DateFormat('yyyy-MM-dd hh:mm:ss');
-    var inputDate = inputFormat.parse(date);
-
-    var outputFormat = DateFormat('MMM yyyy');
-    var outputDate = outputFormat.format(inputDate);
-
-    return outputDate;
-  }
-
-  String formatDateStringToDDMMMMMYYYY(String date) {
-    var inputFormat = DateFormat('dd-MM-yyyy');
-    var inputDate = inputFormat.parse(date);
-
-    var outputFormat = DateFormat('dd MMMM yyyy');
-    var outputDate = outputFormat.format(inputDate);
-
-    return outputDate;
   }
 
   String convertUtcToIst(String utcTimestamp) {
@@ -131,8 +43,6 @@ class CommonUtils {
   }
 
   String convertUtcToIstFormatStringToDDMMYYYYHHMMA(String date) {
-    // DateTime dateTime = DateTime.parse(date);
-
     DateTime utcDateTime = DateTime.parse(date);
     DateTime istDateTime =
         utcDateTime.add(const Duration(hours: 5, minutes: 30));
