a882ed1e2655365f9a62c2d31b007fe730e905da
Ankit Malaviya <amalaviya@pragetx.com>
2025-02-19 14:25:11 +0530

    handle connectivity issue

--- lib/api_services/network_info.dart
+++ lib/api_services/network_info.dart
@@ -1,25 +1,32 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get/get.dart';
+
 import '../helper_files/ui_utils.dart';
 
 abstract class NetworkInfo {
   static Future<bool> checkNetwork() async {
     var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult == ConnectivityResult.mobile) {
-      Get.closeAllSnackbars();
-      return true;
-    } else if (connectivityResult == ConnectivityResult.wifi) {
-      Get.closeAllSnackbars();
-      return true;
-    } else {
-      if (connectivityResult == ConnectivityResult.none) {
-        AppUtils.hideProgressDialog();
-        AppUtils.showErrorSnackBar(bodyText: "No internet connection!");
-        checkNetwork();
-        return false;
-      } else {
+    print("fsdkjfdhsfjkshfjksdhfskfjksd");
+    print(connectivityResult);
+    if (connectivityResult.isNotEmpty) {
+      if (connectivityResult[0] == ConnectivityResult.mobile) {
+        Get.closeAllSnackbars();
         return true;
+      } else if (connectivityResult[0] == ConnectivityResult.wifi) {
+        Get.closeAllSnackbars();
+        return true;
+      } else {
+        if (connectivityResult[0] == ConnectivityResult.none) {
+          AppUtils.hideProgressDialog();
+          AppUtils.showErrorSnackBar(bodyText: "No internet connection!");
+          // checkNetwork();
+          return false;
+        } else {
+          return true;
+        }
       }
+    } else {
+      return false;
     }
   }
 }
